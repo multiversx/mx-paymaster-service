79b7a9a7719bdd3858ad4bddfc0a87364b7c39e0
hschiau <horia.schiau@multiversx.com>
2023-11-27 16:46:09 +0200

    use NODE_ENV at runtime for choosing the correct file

--- apps/api/config/configuration.ts
+++ apps/api/config/configuration.ts
@@ -2,10 +2,10 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
-const YAML_CONFIG_FILENAME = 'config.yaml';
+const YAML_CONFIG_FILENAME = `config.${process.env.NODE_ENV}.yaml`;
 
 export default () => {
-    return yaml.load(
-        readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
-    ) as Record<string, any>;
+  return yaml.load(
+    readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+  ) as Record<string, any>;
 };
